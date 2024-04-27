0e3772f956ca2b1ae0a876b135528bb7089a6313
Szombati Olivér <o.szombati@gmail.com>
2024-04-27 20:27:09 +0200

    Restart - home page design is ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,117 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_homework/network/data_source_interceptor.dart';
-import 'package:get_it/get_it.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-//DO NOT MODIFY
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await configureFixDependencies();
-  await configureCustomDependencies();
+void main() {
   runApp(const MyApp());
-}
-
-//DO NOT MODIFY
-Future configureFixDependencies() async {
-  var dio = Dio();
-  dio.interceptors.add(
-    PrettyDioLogger(
-      requestHeader: true,
-      requestBody: true,
-      responseHeader: true,
-    ),
-  );
-  dio.interceptors.add(DataSourceInterceptor());
-  GetIt.I.registerSingleton(dio);
-  GetIt.I.registerSingleton(await SharedPreferences.getInstance());
-  GetIt.I.registerSingleton(<NavigatorObserver>[]);
-}
-
-//Add custom dependencies if necessary
-Future configureCustomDependencies() async {
-
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Star Wars Characters',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: true,
+        primaryColor: Colors.white,
       ),
-      //DO NOT MODIFY
-      navigatorObservers: GetIt.I<List<NavigatorObserver>>(),
-      //DO NOT MODIFY
-      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: HomePage('Main Page'),
+      ),
     );
   }
 }
+
+class HomePage extends StatelessWidget{
+  final String description;
+  const HomePage(this.description);
+
+  // @override
+  // Widget build(BuildContext context){
+  //   return Center(
+  //     child: Image.asset(
+  //       'assets/lightsabers.png',
+  //       fit: BoxFit.cover,
+  //       width: MediaQuery.of(context).size.width * 0.15,
+  //     ),
+  //   );
+
+  static const Color bgTopColor = Color(0xFF1D6D38);
+  static const Color bgBotColor = Color(0xFF282828);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [bgTopColor, bgBotColor],
+            stops: [0.0, 0.25],
+          ),
+        ),
+        padding: const EdgeInsets.all(80.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/lightsabers.png',
+                fit: BoxFit.fitWidth,
+                width: MediaQuery.of(context).size.width * 0.25,
+              ),
+            ),
+            const SizedBox(height: 20), // Add spacing between image and text
+            const Text(
+              'Characters',
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                child: ListView.separated(
+                  itemCount: 5,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: const Text(
+                        'Example',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onTap: () {},
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.info_outline),
+                            color: Colors.white,
+                          ),
+                          IconButton(
+                            onPressed: (){},
+                            icon: const Icon(Icons.arrow_right),
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) => const Divider(color: Colors.white,),
+                ),
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  }
